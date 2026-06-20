c6f38841b1090f4237b608bfab718e62458da821
alexansh <saraswatasnh4@gmai;.com>
2026-06-20 21:00:28 +0530

    Add OOPS

---
+++ week_3/OOOPS/FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass extends Compartment {
+    @Override
+    public String notice() {
+        return "This is a First Class compartment. Please maintain decorum.";
+    }
+}
---
+++ week_3/OOOPS/Ladies.java
@@ -0,0 +1,6 @@
+public class Ladies extends Compartment {
+    @Override
+    public String notice() {
+        return "This is a Ladies compartment. Only ladies are allowed.";
+    }
+}
